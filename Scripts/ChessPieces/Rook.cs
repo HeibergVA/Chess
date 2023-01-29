58ed2ede3941dcd97ffb029a87f66b23f847e9fd
Sim0nfh <116568592+Sim0nfh@users.noreply.github.com>
2023-01-29 21:31:59 +0100

    Update Rook.cs

--- Scripts/ChessPieces/Rook.cs
+++ Scripts/ChessPieces/Rook.cs
@@ -2,24 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rook : ChessPiece
+public class Rook : ChessPiece // Laver en Rook klasse baseret på chesspiece klassen
 {
     public override List<Vector2Int> GetAvailableMoves(ref ChessPiece[,] board, int tileCountX, int tileCountY)
     {
-        List<Vector2Int> r = new List<Vector2Int>();
+        List<Vector2Int> r = new List<Vector2Int>();  //Laver en liste af vektor
 
         //Ryk ned
-        for (int i = currentY - 1; i >= 0; i--) 
+        for (int i = currentY - 1; i >= 0; i--)   //Laver et for-loop der køre en gang for hvert felt under der hvor brikken står nu
         {
-            if (board[currentX, i]==null)
-                r.Add(new Vector2Int(currentX, i));
-            if (board[currentX, i] != null)
+            if (board[currentX, i]==null)    //Hvis felttet er tomt, altså lig med nul
+                r.Add(new Vector2Int(currentX, i));    //Tilføjer dette felt til listen over lovlige træk
+            if (board[currentX, i] != null)     //Hvis felttet ikke er lig med nul, altså at der står en brik på felttet
             {
-                if(board[currentX, i].team !=team)
-                    r.Add(new Vector2Int(currentX, i));
+                if(board[currentX, i].team !=team)      //Hvis den brik der står på feltet ikke er på dit eget hold
+                    r.Add(new Vector2Int(currentX, i));   //Tilføjer dette felt til listen over lovlige træk
 
-                break;
-
+                break;  //Gør ud af loopet
             }
                 
         }
